db322244e3bba6383f97ea7d91a5db5d346291c3
Humble-writer <95648883+humblewriter01@users.noreply.github.com>
2025-11-25 19:49:05 +0100

    Create apps.tsx

---
+++ apps/frontend/pages/apps.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '../components/layout/DashboardLayout';
+import AppConnections from '../components/AppConnections';
+
+export default function AppsPage() {
+  return (
+    <DashboardLayout>
+      <AppConnections />
+    </DashboardLayout>
+  );
+}
