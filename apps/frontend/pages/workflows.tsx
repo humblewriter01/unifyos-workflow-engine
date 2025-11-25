59cb431e41d03997d2221d6960f279ff3deda691
Humble-writer <95648883+humblewriter01@users.noreply.github.com>
2025-11-25 19:47:59 +0100

    Create workflows.tsx

---
+++ apps/frontend/pages/workflows.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '../components/layout/DashboardLayout';
+import WorkflowBuilder from '../components/WorkflowBuilder';
+
+export default function WorkflowsPage() {
+  return (
+    <DashboardLayout>
+      <WorkflowBuilder />
+    </DashboardLayout>
+  );
+}
