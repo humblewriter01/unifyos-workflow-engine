94a96aca62e316e2f886d250ee4ef7771345dc38
Humble-writer <95648883+humblewriter01@users.noreply.github.com>
2025-11-25 16:32:01 +0100

    Create DashboardLayout.tsx

---
+++ apps/frontend/components/layout/DashboardLayout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="flex h-screen bg-gray-50">
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-auto p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+    }
