7677e1cc84354ac1cb21a62df0750c2543d6a557
Humble-writer <95648883+humblewriter01@users.noreply.github.com>
2025-11-29 07:32:20 +0100

    Wrap Component in ThemeProvider

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '../contexts/ThemeContext'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+// Location: apps/frontend/pages/_app.tsx
